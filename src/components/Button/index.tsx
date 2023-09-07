e75bfa03161d5355caac4cbff5cebd7dac2a9789
glllcs <lucasagallo@gmail.com>
2023-09-07 19:36:07 -0300

    Refactor class components to function components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import style from './Button.module.scss'
 
 interface Props {
-    children: string
-    type?: "button" | "submit" | "reset" | undefined
+    children?: React.ReactNode,
+    type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void
 }
 
-export default class Button extends React.Component<Props> {
-    render() {
-        const { type = "button" } = this.props;
-        return (
-            <button 
-                type={type}
-                className={style.button}
-                onClick={this.props.onClick}
-            >
-                {this.props.children}
-            </button>
-        )
-    }
+export default function Button({ children, type, onClick }: Props) {
+    return (
+        <button 
+            type={type}
+            className={style.button}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
 }
