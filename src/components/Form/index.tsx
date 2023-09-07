2b243d8729127de3944534b1c07d9fd3946e2d94
LadyBobb <134429069+LadyBobb@users.noreply.github.com>
2023-09-07 01:16:06 -0300

    Add state on Form class component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,9 +3,18 @@ import Button from "../Button";
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+    state = {
+        task: "",
+        time: "00:00",
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Add a new study
@@ -14,6 +23,8 @@ class Form extends React.Component {
                         type="text"
                         name="task"
                         id="task"
+                        value={this.state.task}
+                        onChange={event => this.setState({...this.state, task: event.target.value})}
                         placeholder="What do you want to study?"
                         required
                     />
@@ -27,6 +38,8 @@ class Form extends React.Component {
                         step="1"
                         name="time"
                         id="time"
+                        value={this.state.time}
+                        onChange={event => this.setState({...this.state, time: event.target.value})}
                         min="00:00:00"
                         max="01:30:00"
                         required
