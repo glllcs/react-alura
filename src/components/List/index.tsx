67061424ef5926d0785df5c649b84180bfe7dc3a
glllcs <lucasagallo@gmail.com>
2023-09-07 16:33:58 -0300

    Implement feature for updating tasks list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,29 +1,12 @@
 import { useState } from "react";
+import { ITask } from "../../types/task";
 import Item from "./Item";
 import style from './List.module.scss';
 
-function List() {
-    const [tasks, setTasks] = useState([{
-        task: 'React',
-        time: '02:00:00'
-    }, {
-        task: 'Django',
-        time: '01:00:00'
-    }, {
-        task: 'Machine learning',
-        time: '01:00:00'
-    }, {
-        task: 'Rust',
-        time: '01:00:00'
-    }, {
-        task: 'Data Science',
-        time: '01:00:00'
-    }]);
+function List({ tasks }: { tasks: ITask[]}) {
     return (
         <aside className={style.tasksList}>
-            <h2 onClick={() => {
-                setTasks([...tasks, {task: 'Cyber Security', time:'02:00:00'}])
-            }}> Daily studies </h2>
+            <h2> Daily studies </h2>
             <ul>
                 {tasks.map((item, index) => (
                     <Item
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
 import Stopwatch from '../components/Stopwatch';
+import { ITask } from '../types/task';
 import style from './App.module.scss';
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
+      <Form setTasks={setTasks}/>
+      <List tasks={tasks}/>
       <Stopwatch />
     </div>
   );
